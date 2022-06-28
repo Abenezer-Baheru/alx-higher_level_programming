63813c3e6606f7b2bf6f7b5d7502e2e2d0aeb1ab
Abenezer-Baheru <abeniug2001@gmail.com>
2022-06-28 03:26:08 -0700

    Task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-     if chr(i) != 'q' and chr(i) != 'e':
-         print("{}".format(chr(i)), end="")
+for sletter in range(97, 123):
+    if chr(sletter) is not 'q' and chr(sletter) is not 'e':
+        print("{}".format(chr(sletter)), end="")
