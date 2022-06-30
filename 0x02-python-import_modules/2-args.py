943180cb031fa56d13e185b36c19fde959a1c936
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2022-06-30 18:45:25 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len = len(sys.argv) - 1
-    print("{} argument{}{}"
-         .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
-    i = 1
-    while i <= len:
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+
+    length = (len(sys.argv) - 1)
+
+    if length == 1:
+        print("1 argument:")
+    elif length == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(length))
+
+    for x in range(1, len(sys.argv)):
+        print("{:d}:".format(x), str(sys.argv[x]))
