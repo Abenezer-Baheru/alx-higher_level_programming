6fcfdb9663a66d1fa3f348965c3fd57cd73c84fd
Abenezer-Baheru <abeniug2001@gmail.com>
2022-06-30 08:22:10 -0700

    Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     len = len(sys.argv) - 1
     print("{} argument{}{}"
-            .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
+         .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
     i = 1
     while i <= len:
         print("{}: {}".format(i, sys.argv[i]))
