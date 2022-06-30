8a8b81b3dedc343a4ed63ac619af2681dbd78925
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2022-06-30 18:09:07 +0300

    Delete 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    len = len(sys.argv) - 1
-    sum = 0
-    for i in range(len):
-        sum += int(sys.argv[i + 1])
-    print(sum)
