996f7a6119f81d2160e7582ee11849f1fdfd7a66
Abenezer-Baheru <abeniug2001@gmail.com>
2022-07-06 06:34:20 -0700

    Task-11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-        return sum([x[0] * x[1] for x in my_list]) / \
-                        (sum([x[1] for x in my_list]) or 1)
+def multiply_list_map(my_list=[], number=0):
+        return (list(map(lambda x: x * number, my_list)))
