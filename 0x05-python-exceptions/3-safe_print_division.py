05ec7d6098019f0ac7b1518f615bcc3419008cc2
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2022-07-18 14:43:55 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""dividing an intger"""
-
-
 def safe_print_division(a, b):
-    quo = 0
     try:
-        quo = a / b
-    except (ZeroDivisionError):
-        quo = None
+        q = a / b
+    except ZeroDivisionError:
+        q = None
     finally:
-        print("Inside result: {}".format(quo))
-        return 
+        print("Inside result: {}".format(q))
+    return q
