e36f3f0e5b28ef28d489a382cf0fdc18e9d80fa9
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2022-08-02 21:59:47 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Module for a script that adds all arguments to a Python list"""
-import json
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""module to load and save"""
+import sys
 
-file_name = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
-# can also use os.path.exists(file) for try and except
-# be sure to import os.path
-try:
-    json_list = load_from_json_file(file_name)
-except:
-    json_list = []
-for item in argv[1:]:
-    json_list.append(item)
-
-save_to_json_file(json_list, file_name)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
