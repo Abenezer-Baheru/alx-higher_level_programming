138dd6748b3c267f814daa5a2bd5d2de30a2a5f8
Abenezer-Baheru <abeniug2001@gmail.com>
2022-08-02 11:28:02 -0700

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,14 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 filename = 'add_item.json'
 my_list = []
 try:
     my_list = load_from_json_file(filename)
 except Exception:
     save_to_json_file(my_list, filename)
+
 arg_len = len(argv)
 
 if arg_len > 1:
